16a4409768f3ea10afb79769f8427c3c7f80e71a
Almojel-R <86049411+Almojel-R@users.noreply.github.com>
2021-10-31 16:12:41 +0300

    SWE550 demo

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <p>
            Raghad's first github project with travis!
+           SWE 550 demo
           </p>
           <a
             className="App-link"

c23ef7fe40d3962a584cc3b86ed3b70440581a31
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-10-31 14:20:14 +0000

    Deploy Almojel-R/demo-app to github.com/Almojel-R/demo-app.git:gh-pages

---
+++ precache-manifest.4c09b205407a2f6f19761eaf95c7c54d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e96c4fb2b3fd8103cc67",
+    "url": "/demo-app/static/css/main.68a92ed6.chunk.css"
+  },
+  {
+    "revision": "e96c4fb2b3fd8103cc67",
+    "url": "/demo-app/static/js/main.e96c4fb2.chunk.js"
+  },
+  {
+    "revision": "13eeb2032981bf3107f9",
+    "url": "/demo-app/static/js/1.13eeb203.chunk.js"
+  },
+  {
+    "revision": "28fc3e86c9ea21c389e9",
+    "url": "/demo-app/static/js/runtime~main.28fc3e86.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/demo-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "c394a63b1743a58150bed0dfd3886cc9",
+    "url": "/demo-app/index.html"
+  }
+];
